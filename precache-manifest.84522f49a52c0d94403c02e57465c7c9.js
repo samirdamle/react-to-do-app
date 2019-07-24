9532c26f572b47c4dcd119333f133fb3fe366758
Samir Damle <damlemail@gmail.com>
2019-07-24 19:30:24 +0530

    Updates

---
+++ precache-manifest.84522f49a52c0d94403c02e57465c7c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1b99bc9f77c4ef50ddcd9ac2967149f",
+    "url": "/react-to-do-app/index.html"
+  },
+  {
+    "revision": "4b78e2db31c319bf624b",
+    "url": "/react-to-do-app/static/css/2.1f52efde.chunk.css"
+  },
+  {
+    "revision": "5f49d18f548f0ca23e91",
+    "url": "/react-to-do-app/static/css/main.f0be3d66.chunk.css"
+  },
+  {
+    "revision": "4b78e2db31c319bf624b",
+    "url": "/react-to-do-app/static/js/2.53055afa.chunk.js"
+  },
+  {
+    "revision": "5f49d18f548f0ca23e91",
+    "url": "/react-to-do-app/static/js/main.4b033287.chunk.js"
+  },
+  {
+    "revision": "92d52159f3623a56a9a3",
+    "url": "/react-to-do-app/static/js/runtime~main.3e954007.js"
+  }
+]);
